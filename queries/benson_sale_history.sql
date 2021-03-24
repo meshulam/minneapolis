73c0664d03cfd9a57125fcb078c037e9bfc8020d
Matt Meshulam <mattm@reverb.com>
2021-03-24 14:02:35 -0500

    cleanup notebook, fix benson_sale_history.sql

--- queries/benson_sale_history.sql
+++ queries/benson_sale_history.sql
@@ -1,1 +1,9 @@
-select * from lotinfo where apn = '0602823240176';
+select
+  sale_history.*,
+  parcels_2020.*
+from sale_history
+  left join parcels_2020 on sale_history.apn = parcels_2020.APN
+where
+  (SALES_SELLERNAME LIKE '%BENSON%ORTH%'
+    or SALES_BUYERNAME LIKE '%BENSON%ORTH%')
+;
